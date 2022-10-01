ec0fbbf6393ba28404c52d444c23bc7536db49fd
wesley-codes <ezemikey12@gmail.com>
2022-10-01 20:40:44 +0100

    inital commit

--- src/App.js
+++ src/App.js
@@ -2,8 +2,81 @@ import "./App.css";
 import ReactPlayer from "react-player";
 import { Container } from "@material-ui/core";
 import Control from "./Components/Control";
+import { useState, useRef } from "react";
 
 function App() {
+  const videoPlayerRef = useRef(null);
+
+  const [videoState, setVideoState] = useState({
+    playing: true,
+    muted: false,
+    volume: 0.5,
+    playbackRate: 1.0,
+    played: 0,
+    seeking: false,
+  });
+
+  //Destructuring the properties from the videoState
+  const { playing, muted, volume, playbackRate, played, seeking } = videoState;
+
+  const playPauseHandler = () => {
+    //plays and pause the video (toggling)
+    setVideoState({ ...videoState, playing: !videoState.playing });
+  };
+
+  const rewindHandler = () => {
+    //Rewinds the video player reducing 5
+    videoPlayerRef.current.seekTo(videoPlayerRef.current.getCurrentTime() - 5);
+  };
+
+  const handleFastFoward = () => {
+    //FastFowards the video player by adding 10
+    videoPlayerRef.current.seekTo(videoPlayerRef.current.getCurrentTime() + 10);
+  };
+
+  const progressHandler = (state) => {
+    if (!seeking) {
+      setVideoState({ ...videoState, ...state });
+    }
+  };
+
+  const seekHandler = (e, value) => {
+    setVideoState({ ...videoState, played: parseFloat(value / 100) });
+    videoPlayerRef.current.seekTo(parseFloat(value / 100));
+  };
+
+  const seekMouseUpHandler = (e, value) => {
+    console.log(value);
+
+    setVideoState({ ...videoState, seeking: false });
+    videoPlayerRef.current.seekTo(value / 100);
+  };
+
+  const volumeChangeHandler = (e, value) => {
+    const newVolume = parseFloat(value) / 100;
+
+    setVideoState({
+      ...videoState,
+      volume: newVolume,
+      muted: Number(newVolume) === 0 ? true : false, // volume === 0 then muted
+    });
+  };
+
+  const volumeSeekUpHandler = (e, value) => {
+    const newVolume = parseFloat(value) / 100;
+
+    setVideoState({
+      ...videoState,
+      volume: newVolume,
+      muted: newVolume === 0 ? true : false,
+    });
+  };
+
+  const muteHandler = () => {
+    //Mutes the video player
+    setVideoState({ ...videoState, muted: !videoState.muted });
+  };
+
   return (
     <div className="video_container">
       <div>
@@ -12,14 +85,30 @@ function App() {
       <Container maxWidth="md" justify="center">
         <div className="player__wrapper">
           <ReactPlayer
+            ref={videoPlayerRef}
             className="player"
             url="https://bucket-viewer.s3.amazonaws.com/viewer1664370329252.mp4"
             width="100%"
             height="100%"
-            playing={true}
-            muted={true}
+            playing={playing}
+            volume = {volume}
+            muted={muted}
+            onProgress={progressHandler}
           />
-          <Control />
+          <Control
+            onPlayPause={playPauseHandler}
+            playing={playing}
+            onRewind={rewindHandler}
+            onForward={handleFastFoward}
+            played={played}
+            onSeek={seekHandler}
+            onSeekMouseUp={seekMouseUpHandler}
+            volume ={volume}
+            onVolumeChangeHandler = {volumeChangeHandler}
+            onVolumeSeekUp = {volumeSeekUpHandler}
+            mute = {muted}
+            onMute = {muteHandler}
+          />
         </div>
       </Container>
     </div>
--- src/Components/Control.jsx
+++ src/Components/Control.jsx
@@ -1,6 +1,12 @@
 import React from "react";
-import { makeStyles, Slider, withStyles, Button,  Tooltip,  Popover,Grid
-
+import {
+  makeStyles,
+  Slider,
+  withStyles,
+  Button,
+  Tooltip,
+  Popover,
+  Grid,
 } from "@material-ui/core";
 import {
   FastForward,
@@ -8,8 +14,8 @@ import {
   Pause,
   PlayArrow,
   SkipNext,
-  
   VolumeUp,
+  VolumeOff
 } from "@material-ui/icons";
 import "./Control.css";
 
@@ -22,7 +28,6 @@ const useStyles = makeStyles({
   bottomIcons: {
     color: "#999",
     padding: "12px 8px",
-   
 
     "&:hover": {
       color: "#fff",
@@ -64,11 +69,23 @@ const PrettoSlider = withStyles({
   },
 })(Slider);
 
-const Control = () => {
+const Control = ({
+  onPlayPause,
+  playing,
+  onRewind,
+  onForward,
+  played,
+  onSeek,
+  onSeekMouseUp,
+  onVolumeChangeHandler,
+  onVolumeSeekUp,
+  volume,
+  mute,
+  onMute
+}) => {
   const classes = useStyles();
 
-  const playBackRate = [0.5, 1, 1.5, 2]
-
+  const playBackRate = [0.5, 1, 1.5, 2];
 
   return (
     <div className="control_Container">
@@ -76,81 +93,85 @@ const Control = () => {
         <h2>Video PLayer</h2>
       </div>
       <div className="mid__container">
-        <div className="icon__btn">
+        <div className="icon__btn" onDoubleClick={onRewind}>
           <FastRewind fontSize="medium" />
         </div>
 
-        <div className="icon__btn">
-          <Pause fontSize="medium" />
+        <div className="icon__btn" onClick={onPlayPause}>
+          {playing ? (
+            <Pause fontSize="medium" />
+          ) : (
+            <PlayArrow fontSize="medium" />
+          )}{" "}
         </div>
 
         <div className="icon__btn">
-          <FastForward fontSize="medium" />
+          <FastForward fontSize="medium" onDoubleClick={onForward} />
         </div>
       </div>
       <div className="bottom__container">
         <div className="slider__container">
           <PrettoSlider
-          
+            min={0}
+            max={100}
+            value={played * 100}
+            onChange={onSeek}
+            onChangeCommitted={onSeekMouseUp}
           />
         </div>
         <div className="control__box">
           <div className="inner__controls">
-            <div className="icon__btn">
-              <PlayArrow fontSize="medium" />
+            <div className="icon__btn" onClick={onPlayPause}>
+              {playing ? (
+                <Pause fontSize="medium" />
+              ) : (
+                <PlayArrow fontSize="medium" />
+              )}{" "}
             </div>
 
             <div className="icon__btn">
               <SkipNext fontSize="medium" />
             </div>
 
-            <div className="icon__btn">
-              <VolumeUp fontSize="medium" />
+            <div className="icon__btn" onClick={onMute}>
+            {mute ? (
+                  <VolumeOff fontSize="medium" />
+                ) : (
+                  <VolumeUp fontSize="medium" />
+                )}
             </div>
 
             <Slider
               className={`${classes.volumeSlider}`}
-
+              onChange={onVolumeChangeHandler}
+              value={volume * 100}
+              onChangeCommitted={onVolumeSeekUp}
             />
 
             <span>5/20</span>
           </div>
           <div className="second__control">
-            <Button
-              variant="text"
-              className={`${classes.bottomIcons}`}
-            >
-              <span >
-                {" "}
-                4x
-              </span>
+            <Button variant="text" className={`${classes.bottomIcons}`}>
+              <span> 4x</span>
             </Button>
             <Popover
-                anchorOrigin={{
-                  vertical: "top",
-                  horizontal: "center",
-                }}
-                transformOrigin={{
-                  vertical: "bottom",
-                  horizontal: "center",
-                }}
-              >
-                <Grid container direction="column-reverse">
-                  {playBackRate.map((rate) => (
-                    <Button
-                      variant="text"
-                    >
-                      <span
-                         key={rate}
-                      >
-                        {" "}
-                        {rate}x
-                      </span>
-                    </Button>
-                  ))}
-                </Grid>
-              </Popover>
-
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "center",
+              }}
+              transformOrigin={{
+                vertical: "bottom",
+                horizontal: "center",
+              }}
+            >
+              <Grid container direction="column-reverse">
+                {playBackRate.map((rate) => (
+                  <Button variant="text">
+                    <span key={rate}> {rate}x</span>
+                  </Button>
+                ))}
+              </Grid>
+            </Popover>
           </div>
         </div>
       </div>
